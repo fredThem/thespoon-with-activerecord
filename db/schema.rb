af93bb11070a8bfdcdf49648fd911867db1ee5e2
Frederic Desmarais <fdesmarais.them@gmail.com>
2021-02-12 18:59:39 -0500

    schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_212_233_257) do
-  create_table 'restaurants', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'rating'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'address'
+ActiveRecord::Schema.define(version: 2021_02_12_233257) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
+
 end
