6b399657d19cd45872e0cced69539b0cdeaf8226
Frederic Desmarais <fdesmarais.them@gmail.com>
2021-02-13 08:43:09 -0500

    seeds

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+  
+  def index 
+    @restaurants = Restaurant.all
+  end
+  
+  def show 
+    # @restaurant = Restaurant.find(params[:id])
+    # raise
+  end
+  
+  def new 
+    
+  end
+  
+  def create 
+    
+  end
+  
+  def edit 
+    
+  end
+  
+  def create 
+    
+  end
+  
+  def update 
+    
+  end
+  
+  def destroy 
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # CRUD
 
-  verb 'path', to: 'controller#action'
-  read all
-  get '/restaurants', to: 'restaurants#index'
-  # read one
-  get '/restaurants/:id', to: 'restaurants#show'
-  # create a restaurants
-  get '/restaurants/new', to: 'restaurants#new' # display the format
-  post '/restaurants', to: 'restaurants#create'
+  # verb 'path', to: 'controller#action'
+  # # read all
+  # get '/restaurants', to: 'restaurants#index'
+  # # read one
+  # get '/restaurants/:id', to: 'restaurants#show'
+  # # create a restaurants
+  # get '/restaurants/new', to: 'restaurants#new' # display the format
+  # post '/restaurants', to: 'restaurants#create'
 
-  get '/restaurants/:id/edit', to: 'restaurants#edit'
-  path '/restaurants/:id', to: 'restaurants#updates'
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#updates'
 
-  delete 'restaurants/:id', to: 'restaurants#destroy'
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
 
   resources :restaurants
 end
